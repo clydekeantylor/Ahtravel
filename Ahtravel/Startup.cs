302a853c48cf1560f7c316fd93420a4b329fca5b
reymark18 <reymarkdelena@mergetechsolutions.com>
2016-06-06 18:57:18 +0800

    Project Upload

---
+++ Ahtravel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ahtravel.Startup))]
+namespace Ahtravel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
